a31f9a90fee5508aacef807895e4594178989428
santowsa <33058907+santowsa@users.noreply.github.com>
2019-05-28 11:33:17 +0200

    final?
    some error with panda/numpy
    
    could it be possible an error in the installation?

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 # Exercise 3
 from pathlib import Path
-# import functions from utils here
 
-data_dir = Path("data")
-output_dir = Path("solution")
+# import functions from utils here
+import utils as uts
+
+mainDir = Path("C:/Users/Admin/Documents/GitHub/exercise-3-santowsa/")
+data_dir = mainDir / "data"
+output_dir = mainDir / "solution"    
 
 # 1. Contstruct the path to the text file in the data directory using the `pathlib` module [2P]
-
+carPath = data_dir / "cars.txt"
 
 # 2. Read the text file [2P]
 
+with open(carPath,"r") as file:
+    lines = file.read().splitlines()
 
 # 3. Count the occurences of each item in the text file [2P]
 
+uts.countSingles(lines)
 
 # 4. Using `pathlib` check if a directory with name `solution` exists and if not create it [2P]
+
+uts.dirCreate(output_dir)
 
 
 # 5. Write the counts to the file `counts.csv` in the `solution` directory in the format (first line is the header): [2P]
@@ -22,3 +30,8 @@ output_dir = Path("solution")
 #    item_name_1, item_count_1
 #    item_name_2, item_count_2
 #    ...
+import pandas as pd
+
+output = uts.countSingles(lines,returnval=True)
+outPath = output_dir / "counts.csv"
+output.to_csv(outPath, header=True)
--- utils.py
+++ utils.py
@@ -2,3 +2,48 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+def countSingles(x,returnval=False):
+    import pandas as pd
+    """
+    Count the sum of each single item in a list.
+    
+    Parameter:
+    x : list or any format readable in python
+        
+    Returns:
+    Printed total sum for each unique element in the list.
+    If returnval = T
+        Return will be saved in a variable.
+    
+    """
+    if returnval:
+        items = list(set(x))
+        counts = []
+        for value in items: 
+            counts.append(x.count(value))
+            countedDB = pd.DataFrame({'car_name': items, 'number': counts})
+            return(countedDB)
+    else:
+        items = list(set(x))
+        for value in items: 
+            print(value, x.count(value))
+
+def dirCreate(x):
+    """
+    Creates a subdirectory.
+    
+    Parameters:
+    Preferenced subfolder to be createed.
+    
+    Return:
+    Subfolderdirectory creation or a message that it already exists.
+    
+    """
+    subdir = x.exists()
+    if subdir:
+        print("All good! Filepath {} already exists.".format(x))
+    
+    else:
+        x.mkdir(parents = True, exist_ok = True)
+        
+
